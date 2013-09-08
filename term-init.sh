29d564c204617615ae054889ce0b0e3b3993c3a4
Ryuinferno <ryuinferno.xda@gmail.com>
2013-09-08 17:40:32 +1000

    Major overhaul for Android 4.3
    Superuser might be utilising install-recovery.sh as well, therefore using
    install-recovery-2.sh as recommended by Chainfire. This update makes the
    script run a lot faster, more error checks, and utilises sysinit like AOSP ROMs.

--- term-init.sh
+++ term-init.sh
@@ -1,9 +1,30 @@
 #!/system/bin/sh
-#Script to enable init.d by Ryuinferno @ XDA 2013
+#Script to enable init.d by Ryuinferno @ XDA
 
-echo "Init.d Enabler by Ryuinferno @ XDA 2013"
+error_msg(){
+echo "You do not need this mod..."
+sleep 1
+echo "If you are reapplying, please delete these files if present:"
+echo "/system/bin/sysinit"
+sleep 1
+echo "/system/etc/install-recovery.sh"
+sleep 1
+echo "/system/etc/install-recovery-2.sh"
+sleep 1
+echo "And run again..."
+sleep 1
+echo "If init.d is still not working, read the FAQ part in my thread..."
+sleep 1
+echo "Aborting..."
+mount -o remount,ro -t auto /system
 echo ""
-sleep 2
+echo "Ryuinferno @ XDA"
+exit 1
+}
+
+echo "Init.d Enabler by Ryuinferno @ XDA"
+echo ""
+sleep 1
 
 id=`id`; 
 id=`echo ${id#*=}`; 
@@ -11,55 +32,55 @@ id=`echo ${id%%\(*}`;
 id=`echo ${id%% *}`
 if [ "$id" != "0" ] && [ "$id" != "root" ]; then
 	echo "Script NOT running as root!"
-	sleep 2
-	echo "SuperUser access not granted!"
-	sleep 2
+	sleep 1
+	echo "Superuser access not granted!"
+	sleep 1
 	echo "Please type 'su' first before running this script..."
-	exit
+	exit 1
 else
 	echo "Hello Supaa User! :P"
 	echo ""
-	sleep 2
+	sleep 1
 fi
 
 if [ ! "'which busybox'" ]; then
 	echo "busybox NOT INSTALLED!"
-	sleep 2
+	sleep 1
 	echo "Please install busybox first!"
-	exit
+	exit 1
 else
 	echo "busybox found!"
-	sleep 2
+	sleep 1
 fi
 
 bbb=0
 
-if [ ! "`which awk`" ]; then 
+if [ ! "`which grep`" ]; then 
 	bbb=1
-	echo "awk applet NOT FOUND!"
-	sleep 2
+	echo "grep applet NOT FOUND!"
+	sleep 1
 	else 
-	echo "Awesome! awk found! :D"
-	sleep 2
+	echo "Awesome! grep found! :D"
+	sleep 1
 fi
 
 if [ ! "`which run-parts`" ]; then 
 	bbb=1
 	echo "run-parts applet NOT FOUND!"
-	sleep 2
+	sleep 1
 	else
 	echo "Good! run-parts found! :)"
 	echo ""
-	sleep 2
+	sleep 1
 fi
 
 if [ $bbb -eq 1 ] ; then
 	echo ""
 	echo "Required applets are NOT FOUND!"
 	echo ""
-	sleep 2
+	sleep 1
 	echo "Please reinstall busybox!"
-	exit
+	exit 1
 fi
 
 echo "Great! Let's proceed..."
@@ -69,123 +90,162 @@ echo "Press enter to continue..."
 read enterKey
 
 clear
-sleep 2
+sleep 1
 echo "Mounting system as rewritable..."
-echo ""
-busybox mount -o remount,rw -t auto /system
+mount -o remount,rw -t auto /system
 
-sleep 2
-echo "Checking for the presence of install-recovery.sh..."
-sleep 2
-if [ -e /system/etc/install-recovery.sh ]; then
-	echo "Install-recovery.sh found, adding lines to it..."
-	echo "# init.d support" >> /system/etc/install-recovery.sh
-	echo "run-parts /system/etc/init.d/" >> /system/etc/install-recovery.sh
-	echo "" >> /system/etc/install-recovery.sh
-	awk '!x[$0]++' /system/etc/install-recovery.sh > /sdcard/install-recovery.sh
-	cat /sdcard/install-recovery.sh > /system/etc/install-recovery.sh
-	echo "" >> /system/etc/install-recovery.sh
-	rm /sdcard/install-recovery.sh
+sleep 1
+echo ""
+echo "Checking for the presence of sysinit in /system/bin..."
+sleep 1
+if [ -e /system/bin/sysinit ]; then
+	echo "sysinit found..."
+	if [ -z "`cat /system/bin/sysinit | grep "init.d"`" ]; then
+		echo "Adding lines to sysinit..."
+		echo "" >> /system/bin/sysinit
+		echo "# init.d support" >> /system/bin/sysinit
+		echo "" >> /system/bin/sysinit
+		echo "export PATH=/sbin:/system/sbin:/system/bin:/system/xbin" >> /system/bin/sysinit
+		echo "run-parts /system/etc/init.d" >> /system/bin/sysinit 
+		echo "" >> /system/bin/sysinit
+	else
+		echo ""
+		echo "Your sysinit should already be running the scripts in init.d folder at boot..."
+		error_msg
+	fi
 else
-	echo "Install-recovery.sh not found, creating the file..."
+	echo "sysinit not found, creating file..."
+	echo "#!/system/bin/sh" > /system/bin/sysinit
+	echo "# init.d support" >> /system/bin/sysinit
+	echo "" >> /system/bin/sysinit
+	echo "export PATH=/sbin:/system/sbin:/system/bin:/system/xbin" >> /system/bin/sysinit
+	echo "run-parts /system/etc/init.d" >> /system/bin/sysinit 
+	echo "" >> /system/bin/sysinit
+fi
+
+sleep 1
+echo "Setting correct permissions and ownership for sysinit..."
+chmod 755 /system/bin/sysinit
+chown 0.2000 /system/bin/sysinit
+
+sleep 1
+echo ""
+echo "Checking for the presence of install-recovery.sh..."
+sleep 1
+if [ -f /system/etc/install-recovery.sh ] && [ -z "`cat /system/etc/install-recovery.sh | grep "daemon"`" ]; then
+	if [ ! -z "`cat /system/etc/install-recovery.sh | grep "init.d"`" ];then
+		echo "Your install-recovery.sh seems to be already modified for init.d..."
+		error_msg
+	fi
+	echo "install-recovery.sh found, renaming it as install-recovery-2.sh..."
+	mv /system/etc/install-recovery.sh /system/etc/install-recovery-2.sh
+	echo "Recreating install-recovery.sh..."
 	echo "#!/system/bin/sh" > /system/etc/install-recovery.sh
 	echo "# init.d support" >> /system/etc/install-recovery.sh
-	echo "run-parts /system/etc/init.d/" >> /system/etc/install-recovery.sh
+	echo "" >> /system/etc/install-recovery.sh
+	echo "/system/bin/sysinit" >> /system/etc/install-recovery.sh
+	echo "" >> /system/etc/install-recovery.sh
+	echo "# excecuting extra commands" >> /system/etc/install-recovery.sh
+	echo "/system/etc/install-recovery-2.sh" >> /system/etc/install-recovery.sh
+	echo "" >> /system/etc/install-recovery.sh
+elif [ -f /system/etc/install-recovery.sh ] && [ ! -z "`cat /system/etc/install-recovery.sh | grep "daemon"`" ]; then
+	if [ -f /system/etc/install-recovery-2.sh ] && [ ! -z "`cat /system/etc/install-recovery-2.sh | grep "init.d"`" ];then
+		echo "Your install-recovery-2.sh seems to be already modified for init.d..."
+		error_msg
+	fi
+	echo "install-recovery.sh is used for superuser, using install-recovery-2.sh instead..."
+	if [ -f /system/etc/install-recovery-2.sh ]; then
+		echo "" >> /system/etc/install-recovery-2.sh
+		echo "# init.d support" >> /system/etc/install-recovery-2.sh
+		echo "/system/bin/sysinit" >> /system/etc/install-recovery-2.sh
+		echo "" >> /system/etc/install-recovery-2.sh
+	else
+		echo "#!/system/bin/sh" > /system/etc/install-recovery-2.sh
+		echo "# init.d support" >> /system/etc/install-recovery-2.sh
+		echo "" >> /system/etc/install-recovery-2.sh
+		echo "/system/bin/sysinit" >> /system/etc/install-recovery-2.sh
+		echo "" >> /system/etc/install-recovery-2.sh
+	fi
+	if [ -z "`cat /system/etc/install-recovery.sh | grep "install-recovery-2.sh"`" ]; then
+		echo "" >> /system/etc/install-recovery.sh
+		echo "# extra commands" >> /system/etc/install-recovery.sh
+		echo "/system/etc/install-recovery-2.sh" >> /system/etc/install-recovery.sh
+		echo "" >> /system/etc/install-recovery.sh
+	fi
+else
+	echo "install-recovery.sh not found, creating it..."
+	echo "#!/system/bin/sh" > /system/etc/install-recovery.sh
+	echo "# init.d support" >> /system/etc/install-recovery.sh
+	echo "" >> /system/etc/install-recovery.sh
+	echo "/system/bin/sysinit" >> /system/etc/install-recovery.sh
 	echo "" >> /system/etc/install-recovery.sh
 fi
 
-sleep 2
+sleep 1
 echo "Setting the correct permissions and ownership for install-recovery.sh..."
-echo ""
+echo "Also for install-recovery-2.sh if it exists..."
 chmod 755 /system/etc/install-recovery.sh
 chown 0.0 /system/etc/install-recovery.sh
+if [ -f /system/etc/install-recovery-2.sh ]; then
+	chmod 755 /system/etc/install-recovery-2.sh
+	chown 0.0 /system/etc/install-recovery-2.sh
+fi
 
-sleep 2
+sleep 1
+echo ""
 echo "Checking for the presence of the init.d folder..."
-sleep 2
-if [ -e /system/etc/init.d ]; then
-	echo "Init.d folder found..."
+sleep 1
+if [ -d /system/etc/init.d ]; then
+	echo "init.d folder found..."
 else 
-	echo "Init.d folder not found, creating the folder..."
+	echo "init.d folder not found, creating the folder..."
 	mkdir /system/etc/init.d
 fi
 
-sleep 2
+sleep 1
+echo ""
 echo "Creating basic init.d scripts..."
 echo "#!/system/bin/sh" > /system/etc/init.d/08setperm
-echo "#Script to set correct permissions to /system/etc/init.d folder by Ryuinferno @ XDA 2013" >> /system/etc/init.d/08setperm
+echo "#set correct permissions to /system/etc/init.d folder" >> /system/etc/init.d/08setperm
 echo "" >> /system/etc/init.d/08setperm
-echo "busybox mount -o remount,rw -t auto /system;" >> /system/etc/init.d/08setperm
-echo "busybox chmod -R 777 /system/etc/init.d;" >> /system/etc/init.d/08setperm
-echo "busybox mount -o remount,ro -t auto /system;" >> /system/etc/init.d/08setperm
+echo "mount -o remount,rw -t auto /system" >> /system/etc/init.d/08setperm
+echo "chmod -R 777 /system/etc/init.d" >> /system/etc/init.d/08setperm
+echo "mount -o remount,ro -t auto /system" >> /system/etc/init.d/08setperm
 echo "" >> /system/etc/init.d/08setperm
 
 echo "#!/system/bin/sh" > /system/etc/init.d/00test
-echo "#Init.d Test" >> /system/etc/init.d/00test
+echo "#init.d test" >> /system/etc/init.d/00test
 echo "" >> /system/etc/init.d/00test
-echo "if [ -e /data/Test.log ]; then" >> /system/etc/init.d/00test
+echo "if [ -f /data/Test.log ]; then" >> /system/etc/init.d/00test
 echo "rm /data/Test.log" >> /system/etc/init.d/00test
 echo "fi" >> /system/etc/init.d/00test
 echo "" >> /system/etc/init.d/00test
-echo 'echo "Ryuinferno @ XDA 2013" > /data/Test.log' >> /system/etc/init.d/00test
 echo 'echo "Init.d is working !!!" >> /data/Test.log' >> /system/etc/init.d/00test
 echo 'echo "excecuted on $(date +"%d-%m-%Y %r" )" >> /data/Test.log' >> /system/etc/init.d/00test
 echo "" >> /system/etc/init.d/00test
 
-sleep 2
-echo "Setting correct permissions and ownership for init.d folder..."
-echo ""
+sleep 1
+echo "Setting correct permissions and ownership for init.d folder and scipts..."
+chmod 777 /system/etc/init.d
 chmod 777 /system/etc/init.d/08setperm
 chmod 777 /system/etc/init.d/00test
 chown 0.0 /system/etc/init.d
 chown 0.0 /system/etc/init.d/08setperm
 chown 0.0 /system/etc/init.d/00test
 
-sleep 2
-echo "Checking for the presence of sysinit in /system/bin..."
-sleep 2
-if [ -e /system/bin/sysinit ]; then
-	echo "Sysinit found, adding lines to it..."
-	echo "#!/system/bin/sh" >> /system/bin/sysinit
-	echo "# init.d support" >> /system/bin/sysinit
-	echo "" >> /system/bin/sysinit
-	echo "export PATH=/sbin:/system/sbin:/system/bin:/system/xbin" >> /system/bin/sysinit
-	echo "/system/bin/logwrapper run-parts /system/etc/init.d" >> /system/bin/sysinit 
-	echo "" >> /system/bin/sysinit
-	awk '!x[$0]++' /system/bin/sysinit > /sdcard/sysinit
-	cat /sdcard/sysinit > /system/bin/sysinit
-	echo "" >> /system/bin/sysinit
-	rm /sdcard/sysinit
-else
-	echo "Sysinit not found, creating file..."
-	echo "#!/system/bin/sh" > /system/bin/sysinit
-	echo "# init.d support" >> /system/bin/sysinit
-	echo "" >> /system/bin/sysinit
-	echo "export PATH=/sbin:/system/sbin:/system/bin:/system/xbin" >> /system/bin/sysinit
-	echo "/system/bin/logwrapper run-parts /system/etc/init.d" >> /system/bin/sysinit 
-	echo "" >> /system/bin/sysinit
-fi
-
-sleep 2
-echo "Setting correct permissions and ownership for sysinit..."
-chmod 755 /system/bin/sysinit
-chown 0.2000 /system/bin/sysinit
-
-sleep 2
+sleep 1
 echo ""
 echo "Mounting system as read-only..."
-busybox mount -o remount,ro -t auto /system
-sleep 2
+mount -o remount,ro -t auto /system
+sleep 1
 echo ""
 echo "Done!!!"
-sleep 2
+sleep 1
 echo "Please reboot at least twice before checking /data..."
-sleep 2
+sleep 1
 echo "If init.d is working, you will see a Test.log in /data..."
-sleep 2
+sleep 1
 echo ""
 echo "Enjoy!!! =)"
-sleep 2
 echo "Ryuinferno @ XDA 2013"
 exit
-
